e60a8d96801adb41764c90c252e02aa35234a523
Monica Padilla <monicapr858@gmail.com>
2025-11-30 02:45:31 -0600

    Fix CORS y rutas

--- gateway/app.py
+++ gateway/app.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # --- CONFIGURACIÓN HÍBRIDA ---
 CLOUD_URL = os.getenv('BACKEND_URL')
